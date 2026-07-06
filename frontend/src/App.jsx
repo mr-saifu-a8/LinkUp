f3baa27df66533a204ac01f789a173aded51253c
user.name like :  "Mobassir heyat <user.email like : "mhk88142@gmail.com>
2026-07-06 20:01:54 +0530

    made responsive layouts for chatting UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,7 @@
 import { Routes, Route, Navigate } from "react-router-dom";
-<<<<<<< HEAD
 
 import { LoginPage, OnboardingPage, SignupPage, SplashPage, SuccessPage, WelcomePage, ChattingPage } from "./pages/LinkUpPages";
 
-=======
-import SplashPage from "./pages/SplashPage";
-import OnboardingPage from "./pages/OnboardingPage";
-import WelcomePage from "./pages/WelcomePage";
-import LoginPage from "./pages/LoginPage";
-import SignupPage from "./pages/SignupPage";
-import SuccessPage from "./pages/SuccessPage";
-
->>>>>>> f97e8a079325cb89efd6077a55408e8ccad451c6
 export default function App() {
   return (
     <Routes>
@@ -21,10 +11,7 @@ export default function App() {
       <Route path="/login" element={<LoginPage />} />
       <Route path="/signup" element={<SignupPage />} />
       <Route path="/success" element={<SuccessPage />} />
-<<<<<<< HEAD
       <Route path="/chat" element={<ChattingPage />} />
-=======
->>>>>>> f97e8a079325cb89efd6077a55408e8ccad451c6
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
--- frontend/src/pages/ChattingPage.jsx
+++ frontend/src/pages/ChattingPage.jsx
@@ -115,22 +115,23 @@ const ChattingPage = () => {
   ];
 
   return (
-    <div className=' min-w-sm bg-white p-6 md:p-8 flex justify-center items-center'>
-      <div className='h-screen flex flex-col gap-8 '>
+    <div className=' min-w-sm h-auto min-h-auto bg-white p-6 md:p-8 flex justify-center items-center'>
+      <div className=' flex flex-col gap-4 md:gap-8 lg:gap-8'>
         {/* navigation  */}
-        <div className='flex justify-between text-2xl'>
+        <nav className='flex justify-between text-2xl '>
           <ArrowLeft  />
           <div className='flex gap-4'>
             <GrAddCircle className=' text-xl md:text-2xl ' />
             <VscDeviceCameraVideo className=' text-xl md:text-2xl ' />
             <PiDotsThreeCircleThin className=' text-xl md:text-2xl ' />
           </div>
-        </div>
-        {/* search bar and view stories  */}
-        <div className='p-2 rounded-lg bg-gray-100 flex gap-2 '>
+        </nav>
+        {/* search-bar  */}
+        <div className='p-2 rounded-lg bg-gray-100 flex gap-2  '>
           <CiSearch  className=' text-xl md:text-2xl self-center stroke-[1] text-ink-muted ' />
           <input type="text" placeholder='Search' className='text-ink-light outline-none bg-gray-100 placeholder:text-xs md:placeholder:text-sm ' />
         </div>
+        {/* stories of friends */}
         <div className='flex flex-col gap-4' >
           <h2 className=' md:text-lg lg:text-xl '>Recently</h2>
           <div className='flex gap-4'>
@@ -141,12 +142,12 @@ const ChattingPage = () => {
 
         </div>
         {/* messages, requests and inbox */}
-        <div className='flex flex-col gap-4'>
+        <div className='flex flex-col gap-4 '>
           <div className='flex justify-between'>
             <h3 className=' font-medium text-sm lg:text-lg' >Messages</h3>
             <Link to="/login" className=' text-blue-700 font-medium text-sm lg:text-lg' > Requests </Link>
           </div>
-          <div className='max-h-96 no-scrollbar overflow-y-auto scroll-smooth min-w-0 flex-1 flex flex-col gap-4' >
+          <div className='max-h-[550px] md:max-h-[768px] lg:max-h-[600px]  no-scrollbar overflow-y-auto scroll-smooth min-w-0 flex-1 flex flex-col gap-4' >
 
           {chatProfiles.map((item, index) => {
             return <Inbox {...{ item: item }} key={index} />
